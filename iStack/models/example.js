e7da1e043ffa6a15237ad72b3b9cbe57b0985918
Sam Levy <slevy239@gmail.com>
2019-11-16 12:16:59 -0500

    wtf

--- iStack/models/example.js
+++ iStack/models/example.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Example = sequelize.define("query", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
