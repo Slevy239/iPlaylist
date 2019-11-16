15b4a3e9780d447d446e5cada9c7c932a51a3b87
Sam Levy <slevy239@gmail.com>
2019-11-16 13:31:41 -0500

    conflicts?

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("query", {
     question: DataTypes.STRING,
-    answer: DataTypes.TEXT,
+    description: DataTypes.TEXT,
     CreatedAt: DataTypes.DATE
   });
   return Example;
