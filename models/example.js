b883765d88849480bfb0838222da65cd2b9790cd
Sam Levy <slevy239@gmail.com>
2019-11-18 16:22:57 -0500

    home

--- models/example.js
+++ models/example.js
@@ -1,8 +1,8 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Example = sequelize.define("query", {
-//     question: DataTypes.STRING,
-//     description: DataTypes.TEXT,
-//     CreatedAt: DataTypes.DATE
-//   });
-//   return Example;
-// };
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("query", {
+    question: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    CreatedAt: DataTypes.DATE
+  });
+  return Example;
+};
